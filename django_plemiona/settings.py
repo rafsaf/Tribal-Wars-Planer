6fcb612549852fb682b02a3c9ad3607bfe39be2e
Rafał Safin <rafal.safin12@gmail.com>
2020-06-07 22:47:32 +0200

    test

--- django_plemiona/settings.py
+++ django_plemiona/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
+
+
+adsadasdasdas
 """
 
 import os
