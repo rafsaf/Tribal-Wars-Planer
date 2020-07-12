65417185acee111296d3d0e19e628823a7d7fb90
Rafał Safin <rafal.safin12@gmail.com>
2020-07-12 02:08:53 +0200

    settings

--- django_plemiona/settings.py
+++ django_plemiona/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4+f*13imrfgdvxe5xqt_0__6d18gxbb2e(9%3jqh@!_#2$dwn)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
