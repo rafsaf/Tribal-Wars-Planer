7cd7beedcc7a8ccf6e135b882e8670da782bc486
rafsaf <rafal.safin12@gmail.com>
2021-05-10 00:37:32 +0200

    added test_setting for tests and updated tests.yml

---
+++ django_plemiona/test_setting.py
@@ -0,0 +1,13 @@
+from django_plemiona.settings import *
+
+# make tests faster
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
+    }
+}
