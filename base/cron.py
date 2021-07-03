e8dc1fa18f6e2152d1aedef40c3ff9b11832d210
rafsaf <rafal.safin12@gmail.com>
2021-07-03 12:50:36 +0200

    added cron and tests which runs every 10min to delete old outlines

--- base/cron.py
+++ base/cron.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 
 from django.utils.timezone import now
-
+from django.db.models.query import QuerySet
 from utils.database_update import cron_schedule_data_update
 
 from . import models
@@ -15,6 +15,17 @@ def db_update():
 
 def outdate_overviews_delete():
     """Delete expired links"""
-    expiration_data = now() - timedelta(days=30)
-    expired = models.Overview.objects.filter(created__lt=expiration_data)
+    expiration_date = now() - timedelta(days=30)
+    expired = models.Overview.objects.filter(created__lt=expiration_date)
+    expired.delete()
+
+
+def outdate_outline_delete(days: int = 35) -> None:
+    """Delete outlines older than 35 days except test World"""
+    expiration_date = now() - timedelta(days=days)
+    expired: "QuerySet[models.Outline]" = (
+        models.Outline.objects.select_related("world")
+        .filter(created__lt=expiration_date)
+        .exclude(world__postfix="Test")
+    )
     expired.delete()
---
+++ base/tests/test_cron/test_outdate_outline_delete.py
@@ -0,0 +1,68 @@
+from base.models import Outline
+from datetime import timedelta
+from base.tests.test_utils.mini_setup import MiniSetup
+from base.cron import outdate_outline_delete
+
+
+class OutDateOutlineDelete(MiniSetup):
+    def test_does_not_delete_now_outline(self):
+        self.get_outline()
+        count1: int = Outline.objects.count()
+        self.assertEqual(count1, 1)
+
+        outdate_outline_delete()
+
+        count2: int = Outline.objects.count()
+        self.assertEqual(count2, 1)
+
+    def test_does_not_delete_outline_from_30_days(self):
+        outline = self.get_outline()
+        outline.created = outline.created - timedelta(days=30)
+        outline.save()
+
+        count1: int = Outline.objects.count()
+        self.assertEqual(count1, 1)
+
+        outdate_outline_delete()
+
+        count2: int = Outline.objects.count()
+        self.assertEqual(count2, 1)
+
+    def test_does_not_delete_outline_from_35_days(self):
+        outline = self.get_outline()
+        outline.created = outline.created - timedelta(days=35)
+        outline.save()
+
+        count1: int = Outline.objects.count()
+        self.assertEqual(count1, 1)
+
+        outdate_outline_delete()
+
+        count2: int = Outline.objects.count()
+        self.assertEqual(count2, 1)
+
+    def test_DOES_delete_outline_from_36_days(self):
+        outline = self.get_outline()
+        outline.created = outline.created - timedelta(days=36)
+        outline.save()
+
+        count1: int = Outline.objects.count()
+        self.assertEqual(count1, 1)
+
+        outdate_outline_delete()
+
+        count2: int = Outline.objects.count()
+        self.assertEqual(count2, 0)
+
+    def test_DOES_NOT_delete_outline_from_36_days_if_test_world(self):
+        outline = self.get_outline(test_world=True)
+        outline.created = outline.created - timedelta(days=36)
+        outline.save()
+
+        count1: int = Outline.objects.count()
+        self.assertEqual(count1, 1)
+
+        outdate_outline_delete()
+
+        count2: int = Outline.objects.count()
+        self.assertEqual(count2, 1)
