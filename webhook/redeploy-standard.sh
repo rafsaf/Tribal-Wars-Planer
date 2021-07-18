0b622c5653300fec840479ce7c95b54e6f0faa2e
rafsaf <rafal.safin12@gmail.com>
2021-07-18 23:49:15 +0200

    scaled web to 2 containers in redeploy script

--- webhook/redeploy-standard.sh
+++ webhook/redeploy-standard.sh
@@ -4,5 +4,5 @@ cd /home/ubuntu/Tribal-Wars-Planer
 git pull
 docker image prune --force
 set +e
-docker-compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml up -d --build --scale web=2
 docker image prune --force
