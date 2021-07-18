6d6a2f865c63b6c11a0ef2044d838f39c86bc036
rafsaf <rafal.safin12@gmail.com>
2021-07-18 22:55:40 +0200

    updated initial.sh, removed cron stuff

--- initial.sh
+++ initial.sh
@@ -1,11 +1,5 @@
+#!/bin/bash
+
 python manage.py collectstatic
-
 python manage.py migrate
-
 python manage.py createsuperuser --no-input
-
-python manage.py crontab add
-
-python manage.py crontab show
-
-service cron restart
