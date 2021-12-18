f725b56b52ac9abe9b314d931abf24169a4661e5
rafsaf <rafal.safin12@gmail.com>
2021-12-18 15:47:31 +0100

    add terraform and configure staging env

---
+++ webhook/redeploy-stage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /Tribal-Wars-Planer
+
+git fetch
+git reset --hard HEAD
+git merge origin/master
+
+docker image prune --force
+set +e
+docker-compose -f docker-compose.stg.yml up -d --build
+docker image prune --force
