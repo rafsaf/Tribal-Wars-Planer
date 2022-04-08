a2af82da41baebe027ec67dfd89ea6c6fbc5a530
rafsaf <rafal.safin12@gmail.com>
2022-04-08 02:12:27 +0200

    fixed workflows to allow building arm64 architecutre, added workflow to manually push latest image

--- scripts/push_image_twp_cronjobs.sh
+++ scripts/push_image_twp_cronjobs.sh
@@ -1,5 +1,5 @@
 # /bin/bash
 echo "pushing docker twp-cronjobs image with tag $1"
-docker build . -f Dockerfile.prod --tag rafsaf/twp-cronjobs:$1
+docker build --platform linux/amd64,linux/arm64 . -f Dockerfile.cronjobs --tag rafsaf/twp-cronjobs:$1
 docker image tag rafsaf/twp-cronjobs:$1 rafsaf/twp-cronjobs:$1
 docker image push rafsaf/twp-cronjobs:$1
