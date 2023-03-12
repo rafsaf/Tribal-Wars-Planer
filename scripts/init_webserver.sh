aaaa5d04ad73ecf31aaadb4e3337e3431ee01f1b
rafsaf <rafal.safin12@gmail.com>
2023-03-12 09:39:31 +0100

    Add more params to ensure 150s timeout

--- scripts/init_webserver.sh
+++ scripts/init_webserver.sh
@@ -6,7 +6,7 @@
 bash /build/initial.sh
 
 ### 2. Start uwsgi processes ### 
-uwsgi --chdir=/build --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=2 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
+uwsgi --chdir=/build --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=2 --socket-timeout=150 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
 
 ### 3. Start nginx ###
 nginx -g "daemon off;"
