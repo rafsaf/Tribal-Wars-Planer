f8fd282edf57013e953e5af4048da03c07f75f7a
rafsaf <rafal.safin12@gmail.com>
2023-03-16 21:20:44 +0100

    Add index to OutlineWriteLock table

--- base/models/outline_write_lock.py
+++ base/models/outline_write_lock.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class OutlineWriteLock(models.Model):
     outline_id = models.BigIntegerField(primary_key=True)
-    lock_expire = models.DateTimeField()
+    lock_expire = models.DateTimeField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
