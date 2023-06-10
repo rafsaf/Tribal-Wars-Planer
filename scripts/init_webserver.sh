f9dd6c8a7dad49ab5d73342d096a1bc79f9bee3b
rafsaf <rafal.safin12@gmail.com>
2023-06-10 20:37:15 +0200

    Dockerfile run uwsgi server process as uid uwsgi without shell

--- scripts/init_webserver.sh
+++ scripts/init_webserver.sh
@@ -5,8 +5,13 @@
 ### 1. Initial script, migrations etc., cleanup ###
 bash /build/initial.sh
 
+chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/logs
+chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/media
+chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/prometheus_multi_proc_dir
+chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/disk_cache
+
 ### 2. Start uwsgi processes ### 
-uwsgi --chdir=/build --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=2 --socket-timeout=150 --http-timeout=150 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
+uwsgi --chdir=/build --uid=${SERVICE_NAME} --gid=${SERVICE_NAME} --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=2 --socket-timeout=150 --http-timeout=150 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
 
 ### 3. Start nginx ###
 nginx -g "daemon off;"
