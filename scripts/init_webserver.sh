0274accc262120db2b50458dd4ca7f597b98e4dc
rafsaf <rafal.safin12@gmail.com>
2023-06-15 00:36:44 +0200

    uwsgi use 1 thread by default

--- scripts/init_webserver.sh
+++ scripts/init_webserver.sh
@@ -11,7 +11,7 @@ chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/prometheus_multi_proc_dir
 chown -R ${SERVICE_NAME}:${SERVICE_NAME} /build/disk_cache
 
 ### 2. Start uwsgi processes ### 
-uwsgi --chdir=/build --uid=${SERVICE_NAME} --gid=${SERVICE_NAME} --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=2 --socket-timeout=150 --http-timeout=150 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
+uwsgi --chdir=/build --uid=${SERVICE_NAME} --gid=${SERVICE_NAME} --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=${UWSGI_THREADS} --socket-timeout=150 --http-timeout=150 --harakiri=150 --home=/venv/ --daemonize=/build/logs/uwsgi.log
 
 ### 3. Start nginx ###
 nginx -g "daemon off;"
