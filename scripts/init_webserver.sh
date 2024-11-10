141994d230a2c34bdaea31f97186e4d9bb7d1e1d
rafsaf <rafal.safin@rafsaf.pl>
2024-11-10 13:10:12 +0100

    adjust uwsgi to not write errors on broken pipes
    Signed-off-by: rafsaf <rafal.safin@rafsaf.pl>

--- scripts/init_webserver.sh
+++ scripts/init_webserver.sh
@@ -16,7 +16,11 @@ runuser -u ${SERVICE_NAME} -- /usr/bin/nohup python scripts/expose_metrics_serve
 
 ### 3. Start uwsgi processes ### 
 echo "start uwsgi"
-uwsgi --chdir=/build --uid=${SERVICE_NAME} --gid=${SERVICE_NAME} --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=${UWSGI_THREADS} --socket-timeout=360 --http-timeout=360 --harakiri=360 --home=/venv/ --daemonize=/build/logs/uwsgi.log
+uwsgi --chdir=/build --uid=${SERVICE_NAME} --gid=${SERVICE_NAME} \
+    --module=tribal_wars_planer.wsgi:application --master --pidfile=/tmp/project-master.pid \
+    --socket=/tmp/uwsgi.sock --processes=${UWSGI_PROCESSES} --threads=${UWSGI_THREADS} \
+    --socket-timeout=360 --http-timeout=360 --harakiri=360 --home=/venv/ \
+    --daemonize=/build/logs/uwsgi.log --ignore-sigpipe --ignore-write-errors --disable-write-exception
 
 ### 3. Start nginx ###
 echo "start nginx"
