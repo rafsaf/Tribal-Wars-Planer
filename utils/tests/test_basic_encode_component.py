3366df654313f524cb63a4c65a9eb36bd71d67fa
rafsaf <rafal.safin@rafsaf.pl>
2024-12-15 23:28:16 +0100

    fix lint
    Signed-off-by: rafsaf <rafal.safin@rafsaf.pl>

--- utils/tests/test_basic_encode_component.py
+++ utils/tests/test_basic_encode_component.py
@@ -29,6 +29,6 @@ class TestEncodeComponent(TestCase):
             "3. [color=#0e0eff][b]Burzak[/b][/color] (Katapulty-50 [b]Tartak[/b])\n\r\n\t[b]2021-02-20 [color=#ff0000]08:39:12 - 08:39:12[/color][/b]": "3.%20%5Bcolor%3D%230e0eff%5D%5Bb%5DBurzak%5B%2Fb%5D%5B%2Fcolor%5D%20(Katapulty-50%20%5Bb%5DTartak%5B%2Fb%5D)%0A%0D%0A%09%5Bb%5D2021-02-20%20%5Bcolor%3D%23ff0000%5D08%3A39%3A12%20-%2008%3A39%3A12%5B%2Fcolor%5D%5B%2Fb%5D",
         }
 
-        for test_case_name in cases_dict:
+        for test_case_name, val in cases_dict.items():
             case_after_encoding = encode_component(test_case_name)
-            self.assertEqual(cases_dict[test_case_name], case_after_encoding)
+            self.assertEqual(val, case_after_encoding)
