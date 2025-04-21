ddc97c8622e0a3c2685223f2928ea24d5233be14
Rafał Safin <rafal.safin@rafsaf.pl>
2025-04-21 14:37:31 +0200

    add cdist brute benchmark
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

---
+++ utils/tests/test_cdist_brute.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from utils.basic.cdist_brute import CDistBrute
+
+
+def test_cdist_brute(codspeed_benchmark) -> None:
+    ally = []
+    for i in range(1000):
+        for j in range(20):
+            ally.append((i, j))
+    enemy = []
+    for i in range(100):
+        for j in range(6):
+            enemy.append((i, j))
+
+    @codspeed_benchmark
+    def bench():
+        CDistBrute(
+            ally_villages=np.array(ally),
+            enemy_villages=np.array(enemy),
+            min_radius=1.0,
+            max_radius=10.0,
+        ).triple_result()
