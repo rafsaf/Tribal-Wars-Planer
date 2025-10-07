2e21ce11ad38f8e1b099886173c1e136fda7934a
rafsaf <rafal.safin12@gmail.com>
2025-10-07 19:00:23 +0200

    bump to postgres 18
    Signed-off-by: rafsaf <rafal.safin12@gmail.com>

--- tribal_wars_planer/settings.py
+++ tribal_wars_planer/settings.py
@@ -200,6 +200,10 @@ DATABASES: dict[str, dict[str, Any]] = {
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
         "CONN_MAX_AGE": int(os.environ.get("CONN_MAX_AGE", "120")),
         "CONN_HEALTH_CHECKS": True,
+        "OPTIONS": {
+            # Disable JIT https://www.postgresql.org/docs/current/jit.html
+            "options": "-c jit=off",
+        },
     }
 }
 
